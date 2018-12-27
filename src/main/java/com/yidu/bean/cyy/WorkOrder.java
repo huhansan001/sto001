0964b8f9af44ec2fb18a507290d9b5ae45e4970c
Administrator <Administrator@PC-20180425LRVW>
2018-12-27 12:04:49 +0800

    上传人：陈莹莹，模块：寄件人工单表

---
+++ src/main/java/com/yidu/bean/cyy/WorkOrder.java
@@ -0,0 +1,41 @@
+package com.yidu.bean.cyy;
+/**
+ * 描述：工单表类
+ * @author Administrator
+ *
+ */
+public class WorkOrder {
+	private int packageId;//包裹id
+	private String empNo;//员工编号
+	private String sendTime;//派发时间
+	private String worksheet;//工单状态
+	public int getPackageId() {
+		return packageId;
+	}
+	public void setPackageId(int packageId) {
+		this.packageId = packageId;
+	}
+	public String getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
+	public String getSendTime() {
+		return sendTime;
+	}
+	public void setSendTime(String sendTime) {
+		this.sendTime = sendTime;
+	}
+	public String getWorksheet() {
+		return worksheet;
+	}
+	public void setWorksheet(String worksheet) {
+		this.worksheet = worksheet;
+	}
+	@Override
+	public String toString() {
+		return "workOrder [packageId=" + packageId + ", empNo=" + empNo + ", sendTime=" + sendTime + ", worksheet="
+				+ worksheet + "]";
+	}
+}
