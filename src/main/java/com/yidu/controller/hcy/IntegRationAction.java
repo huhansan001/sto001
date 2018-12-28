e425e5550a9f56e8287d014507d72882844a1c60
Administrator <Administrator@PC-20181208SBPG>
2018-12-28 12:25:12 +0800

    修改控制层

--- src/main/java/com/yidu/controller/hcy/IntegRationAction.java
+++ src/main/java/com/yidu/controller/hcy/IntegRationAction.java
@@ -1,8 +1,11 @@
 package com.yidu.controller.hcy;
 
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 
@@ -15,8 +18,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IntegRationAction {
 	
 	@RequestMapping("IntegRation.action")
-	public @ResponseBody String IntegRation() {
+	public void  IntegRation(HttpServletRequest  request,HttpServletResponse response) {
+		/*System.out.println(12321321);
+		ModelAndView mav=new ModelAndView();
+		
+		mav.setViewName("http://localhost:8080/stoMaven/FrontEnd/STO.jsp");
+		return mav;*/
+		//request.getSession().setAttribute("list", "123");
+		//System.out.println(12321321);
+		//try {
+			//response.sendRedirect("http://localhost:8080/stoMaven/FrontEnd/admin/query.jsp");
+		//} catch (IOException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		//}
 		//Integer.parseInt(s)
-		return "≤‚ ‘";
 	}
 }
