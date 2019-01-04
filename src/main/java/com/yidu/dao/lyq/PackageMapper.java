442529991e536648be913b603123cee012fd8105
Administrator <Administrator@3V9GZATXSX7B6O5>
2019-01-04 10:07:11 +0800

    lyq包裹表修改数据库名字

--- src/main/java/com/yidu/dao/lyq/PackageMapper.java
+++ src/main/java/com/yidu/dao/lyq/PackageMapper.java
@@ -24,30 +24,30 @@ public interface PackageMapper {
  * 功能：查询所有的方法
  * @return
  */
-@Select("select * from package ")
+@Select("select * from package_lyq ")
 public List<com.yidu.bean.lyq.Package> selectpackageAll();
 /**
  * 功能：新增的方法
  * @return
  */
-@Insert("insert into package values(#{packageId},#{workOrderNumber},#{nameOfSender},#{senderAddress},#{fullAddressOfSender},#{sendersTelephoneNumber},#{addresseeName},#{direction},#{addresseeDetailedAddress},#{addresseeTelephoneNumber},#{theParcelWeighs},#{freight},#{auditStatus},#{waybillNumber},#{remark})")
+@Insert("insert into package_lyq values(#{packageId},#{workOrderNumber},#{nameOfSender},#{senderAddress},#{fullAddressOfSender},#{sendersTelephoneNumber},#{addresseeName},#{direction},#{addresseeDetailedAddress},#{addresseeTelephoneNumber},#{theParcelWeighs},#{freight},#{auditStatus},#{waybillNumber},#{remark})")
 public int insertpackage(com.yidu.bean.lyq.Package packages);
 /**
  * 功能：分页的方法
  * @return
  */
-@Select("select * from package limit #{page},#{rows}")
+@Select("select * from package_lyq limit #{page},#{rows}")
 public List<com.yidu.bean.lyq.Package> selectpackageAllpage(@Param("page") int page,@Param("rows") int rows);
 /**
  * 功能：根据ID修改的方法
  * @return
  */
-@Update("update package set workOrderNumber=#{workOrderNumber},nameOfSender=#{nameOfSender},senderAddress=#{senderAddress},fullAddressOfSender=#{fullAddressOfSender},sendersTelephoneNumber=#{sendersTelephoneNumber},addresseeName=#{addresseeName},direction=#{direction},addresseeDetailedAddress=#{addresseeDetailedAddress},addresseeTelephoneNumber=#{addresseeTelephoneNumber},theParcelWeighs=#{theParcelWeighs},freight=#{freight},auditStatus=#{auditStatus},waybillNumber=#{waybillNumber},remark=#{remark} where packageId=#{packageId}")
+@Update("update package_lyq set workOrderNumber=#{workOrderNumber},nameOfSender=#{nameOfSender},senderAddress=#{senderAddress},fullAddressOfSender=#{fullAddressOfSender},sendersTelephoneNumber=#{sendersTelephoneNumber},addresseeName=#{addresseeName},direction=#{direction},addresseeDetailedAddress=#{addresseeDetailedAddress},addresseeTelephoneNumber=#{addresseeTelephoneNumber},theParcelWeighs=#{theParcelWeighs},freight=#{freight},auditStatus=#{auditStatus},waybillNumber=#{waybillNumber},remark=#{remark} where packageId=#{packageId}")
 public void updatepackage(com.yidu.bean.lyq.Package packages);
 /**
  * 功能：根据ID删除的方法
  * @return
  */
-@Delete("delete from package where packageId=#{packageId}")
+@Delete("delete from package_lyq where packageId=#{packageId}")
 public void deletepackage(com.yidu.bean.lyq.Package packages);
 }
