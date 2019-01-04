0a164ab9be68b5d1c8f517c44b55c5f7a583c9e8
Administrator <Administrator@PC-20181015LPPQ>
2019-01-04 10:46:13 +0800

    上传：文思晴   工单表

---
+++ src/main/java/com/yidu/controller/wsq/OrderController.java
@@ -0,0 +1,64 @@
+package com.yidu.controller.wsq;
+
+
+import java.util.Map;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yidu.bean.wsq.StoWorkOrder_wsq;
+import com.yidu.service.wsq.OrderService;
+
+@Controller
+public class OrderController {
+	@Autowired
+	private OrderService orderservice;
+	@RequestMapping("selectOrderAll.action")
+	public @ResponseBody Map<String,Object> selectAll(String page,String limit){
+		Map<String, Object> map = orderservice.selectWorkOrder_wsqAll( page,limit);
+		//System.out.println(map);
+		return map;
+	}
+	/**
+	 * 根据id查询
+	 */
+	@RequestMapping("selectid.action")
+	public @ResponseBody Map<String,Object> selectid(StoWorkOrder_wsq workerNumber){
+		System.out.println("-------");
+		System.out.println(workerNumber.toString());
+//		orderservice.selectid(workerNumber);
+		return null;
+	}
+	/**
+	 *增加一个用户
+	 */
+	@RequestMapping("insert.action")
+	public @ResponseBody String insertuser(StoWorkOrder_wsq order) {
+		
+		
+		//System.out.println(order.toString());
+		int	 temp = orderservice.insert(order);
+		System.out.println(temp+"===值"+order);
+		return "添加成功";
+	}
+	/**
+	 * 根据id删除用户
+	 */
+	@RequestMapping("delectOrder.action")
+	public @ResponseBody void delectOrderid (@ModelAttribute StoWorkOrder_wsq orderid) {
+		orderservice.delectOrderid(orderid);
+		//System.out.println(orderid);
+	}
+	/**
+	 * 根据id修改
+	 */
+	@RequestMapping("upOrder.action")
+	public @ResponseBody void upOrderid (@ModelAttribute StoWorkOrder_wsq workorder) {
+		orderservice.uodates(workorder);		
+		//System.out.println(workorder.toString());
+	}
+}
---
+++ src/main/java/com/yidu/dao/wsq/OrderMapper.java
@@ -0,0 +1,53 @@
+package com.yidu.dao.wsq;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
+
+import com.yidu.bean.wsq.StoWorkOrder_wsq;
+
+@Repository
+public interface OrderMapper {
+	
+	/**
+	 *查询所有用户
+	 */
+	@Select ("select * from workorder_wsq")
+	public List<StoWorkOrder_wsq> selectWorkOrder_wsqAll();
+	/**
+	 * 根据ID号查询用户信息
+	 */
+	@Select ("select * from workorder_wsq where workerNumber=#{workerNumber}")
+	public List<StoWorkOrder_wsq> selectid(StoWorkOrder_wsq workerNumber);
+	/**
+	 * 新增一条信息
+	 */
+
+	@Insert("INSERT INTO workorder_wsq (workerNumber,employeeNumber,orderNumber,serviceHours) VALUES (#{workerNumber},#{employeeNumber},#{orderNumber},#{serviceHours})")
+	public int insertplace(StoWorkOrder_wsq user);
+	
+	/**
+	 * 删除一条数据
+	 */
+	@Delete("delete from workorder_wsq where workerNumber=#{workerNumber}")
+	public void delectOrderid (StoWorkOrder_wsq user);
+	
+	/**
+	 * 分页查询
+	 */
+	@Select("SELECT * FROM workorder_wsq LIMIT #{page},#{rows}")
+	public List<StoWorkOrder_wsq> selectWorkOrder_wsqlimit (@Param("page")int page,@Param("rows")int row);
+	
+	/**
+	 * 修改一条数据
+	 */
+	@Update("update workorder_wsq SET employeeNumber=#{employeeNumber},orderNumber=#{orderNumber},serviceHours=#{serviceHours} WHERE workerNumber=#{workerNumber}")
+	public void uodates(StoWorkOrder_wsq workorder);
+	
+}
+	
